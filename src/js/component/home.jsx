80c1c78b3062439646d9d018bf4e731f1e72c668
Borja <borjamese@gmail.com>
2023-04-05 19:37:14 +0000

    f

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,32 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+			//el valor del input
+	const [currentList, setCurrentList] = useState("");
+ 
+	// el estado de los elementos en la lista que he añadido hasta el momento
+	const [elementsInList, setElementsInList] = useState([]);
+
+  return (
+    <div className="home-header">
+      <h1>To-dos</h1>
+	  
+      <input type="text" id="myInput" placeholder="Type here" />
+
+	  <br></br><br></br>
+	  
+	  <ul>
+		
+  <li>Hit the gym</li>
+  <li>Pay bills</li>
+  <li>Meet George</li>
+  <li>Buy eggs</li>
+  <li>Read a book</li>
+  <li>Organize office</li>
+	</ul>
+    </div>
+  );
 };
 
 export default Home;
