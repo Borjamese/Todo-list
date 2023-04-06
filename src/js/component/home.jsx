5665f6c9a278ab56345cbe84a111ea7b9b63f8f0
Borja <borjamese@gmail.com>
2023-04-06 10:32:50 +0000

    finsihed

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,27 +3,36 @@ import React, { useState } from "react";
 //create your first component
 const Home = () => {
 			//el valor del input
-	const [currentList, setCurrentList] = useState("");
+	const [currentWord, setCurrentWord] = useState("");
  
 	// el estado de los elementos en la lista que he añadido hasta el momento
-	const [elementsInList, setElementsInList] = useState([]);
+	const [wordInList, setWordInList] = useState([]);
 
   return (
     <div className="home-header">
       <h1>To-dos</h1>
 	  
-      <input type="text" id="myInput" placeholder="Type here" />
+      <input
+  type="text"
+  id="myInput"
+  onChange={(evento) => setCurrentWord(evento.target.value)}
+  value={currentWord}
+  placeholder="Type here"
+  onKeyDown={(event) => {
+    if (event.key === "Enter") {
+      setWordInList((prevList) => [...wordInList, currentWord]);
+      setCurrentWord("");
+    }
+  }}
+/>
 
 	  <br></br><br></br>
 	  
-	  <ul>
-		
-  <li>Hit the gym</li>
-  <li>Pay bills</li>
-  <li>Meet George</li>
-  <li>Buy eggs</li>
-  <li>Read a book</li>
-  <li>Organize office</li>
+
+	  <ul class="list-group">
+      {wordInList.map ((w) => ( 
+  <li class ="list-group-item">{w} </li>
+      ))}
 	</ul>
     </div>
   );
