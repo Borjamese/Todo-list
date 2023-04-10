0135b46dace22b0bedb5ced26d1ec0b9a8447738
Borja <borjamese@gmail.com>
2023-04-10 11:10:43 +0000

    finish

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,6 +8,10 @@ const Home = () => {
 	// el estado de los elementos en la lista que he añadido hasta el momento
 	const [wordInList, setWordInList] = useState([]);
 
+  const handleDelete = (itemToDelete) => {
+    setWordInList((prevList) => prevList.filter((w) => w !== itemToDelete));
+  };
+
   return (
     <div className="home-header">
       <h1>To-dos</h1>
@@ -30,10 +34,19 @@ const Home = () => {
 	  
 
 	  <ul class="list-group">
-      {wordInList.map ((w) => ( 
-  <li class ="list-group-item">{w} </li>
-      ))}
-	</ul>
+        {wordInList.map((w) => (
+          <li class="list-group-item">
+            {w}{" "}
+            <button
+              aria-label='delete item'
+              type='button'
+              onClick={() => handleDelete(w)}
+            >
+              X
+            </button>{" "}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
